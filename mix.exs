7262a53d67e3455b04b97254b5648c1b2ae86187
c4710n <c4710n@users.noreply.github.com>
2022-08-08 23:59:43 +0800

    chore: relax Elixir version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule CozyParams.MixProject do
     [
       app: :cozy_params,
       version: "0.1.0",
-      elixir: "~> 1.13",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
