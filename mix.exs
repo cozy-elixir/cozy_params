4b7088766ed6cf1e4ba67001dae8715022e78000
c4710n <c4710n@users.noreply.github.com>
2023-01-14 23:37:05 +0800

    deps: add ex_check

--- mix.exs
+++ mix.exs
@@ -32,7 +32,11 @@ defmodule CozyParams.MixProject do
   defp deps do
     [
       {:ecto, "~> 3.0"},
-      {:ex_doc, "~> 0.27", only: :dev, runtime: false}
+      {:ex_check, "~> 0.14.0", only: [:dev], runtime: false},
+      {:credo, ">= 0.0.0", only: [:dev], runtime: false},
+      {:dialyxir, ">= 0.0.0", only: [:dev], runtime: false},
+      {:ex_doc, ">= 0.0.0", only: [:dev], runtime: false},
+      {:mix_audit, ">= 0.0.0", only: [:dev], runtime: false}
     ]
   end
 
