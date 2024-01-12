d8c0e7e365f05c024b529e9e6a8775fbb9770a60
c4710n <c4710n@users.noreply.github.com>
2024-01-12 12:27:40 +0800

    release: bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule CozyParams.MixProject do
   use Mix.Project
 
-  @version "2.1.0"
+  @version "2.1.1"
   @source_url "https://github.com/cozy-elixir/cozy_params"
   @description "Provides Ecto-like API for casting and validating params."
 
